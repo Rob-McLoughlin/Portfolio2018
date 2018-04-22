062b3633d0dac885afe9772f71427cc91779e53a
Rob McLoughlin <robbiemcloughlin@gmail.com>
2018-04-22 23:11:48 +0100

    fixed bug with home square

--- js/home.js
+++ js/home.js
@@ -1,9 +1,18 @@
 var setSquare = function(e) {
   var width = e.offsetWidth;
+  e.style.minHeight = width + 'px';
+}
+var setSquareSmall = function(e) {
+  var width = e.offsetWidth;
   e.style.height = width + 'px';
 }
 
-if (window.innerWidth >= 1440 || window.innerWidth < 1024){
+if (window.innerWidth < 420){
+  var homeSquare = document.getElementById('home-card');
+  setSquareSmall(homeSquare);
+} else if (window.innerWidth < 769){
+
+} else if (window.innerWidth >= 1440 || window.innerWidth < 1024){
 var homeSquare = document.getElementById('home-card');
 setSquare(homeSquare);
 }
